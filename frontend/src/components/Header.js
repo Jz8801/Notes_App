4ed1e299ef36c850211d9cd24feb978999d15acb
Richard Zhong <98974633+Jz8801@users.noreply.github.com>
2024-07-18 03:27:06 -0400

    more styling

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -2,8 +2,8 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <div>
-        <h1>Header</h1>
+    <div className='app-header'>
+        <h1>myNotebook</h1>
     </div>
   )
 }
--- frontend/src/pages/NotePage.js
+++ frontend/src/pages/NotePage.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react'
-import { useParams } from 'react-router-dom'
+import { Link, useParams } from 'react-router-dom'
+import { ReactComponent as ArrowLeft } from '../assets/arrow-left.svg'
 
 const NotePage = ({ match }) => {
 
@@ -17,8 +18,15 @@ const NotePage = ({ match }) => {
     }
 
   return (
-    <div>
-        <p>{note?.body}</p>
+    <div className='note'>
+      <div className='note-header'>
+        <h3>
+          <Link to='/'>
+            <ArrowLeft />
+          </Link>
+        </h3>
+      </div>
+      <textarea defaultValue={note?.body}></textarea>
     </div>
   )
 }
